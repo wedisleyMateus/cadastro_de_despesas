5948285e2076fdb6cdda3a8459ab13b0e1e11aff
wedisleyMateus <wedmat282016@gmail.com>
2024-05-12 21:04:30 -0300

    chore: activating view in admin

--- revenue/admin.py
+++ revenue/admin.py
@@ -4,4 +4,12 @@ from .models import BankingAssociation, RevenueValue, NetRevenue
 class BankingAssociationAdmin(admin.ModelAdmin):
     list_display = ('service', 'value', 'date')
 
+class RevenueValueAdmin(admin.ModelAdmin):
+    list_display = ('fixed_revenue', 'variable_revenue', 'date')
+
+class NetRevenueAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'datetime')
+
 admin.site.register(BankingAssociation, BankingAssociationAdmin)
+admin.site.register(RevenueValue, RevenueValueAdmin)
+admin.site.register(NetRevenue, NetRevenueAdmin)
